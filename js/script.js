23942303fc107944509905f77bc00dc30729ce18
Bankai2301 <ckastrusshion@gmail.com>
2023-12-20 14:04:02 +0300

    Timer-js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const daysEl = document.querySelector("#days");
+const hoursEl = document.querySelector("#hours");
+const minsEl = document.querySelector("#mins");
+const secondsEl = document.querySelector("#seconds");
+
+
+
+const newYear = "1 Jan 2024";
+
+function countDown() {
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+countDown();
+
+setInterval(countDown, 1000)
+
+
+
+
+
